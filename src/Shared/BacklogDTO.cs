6d0abfe1f72645bd2ae45f2d82ead7f69706a6da
Александр Морозов <morozov@ilabsltd.com>
2020-06-12 10:33:00 +0400

    Initial commit

---
+++ src/Shared/BacklogDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Agile4SMB.Shared
+{
+    public class BacklogDTO
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public OrganizationUnitDTO OrganizationUnit { get; set; }
+    }
+}
---
+++ src/Shared/OrganizationUnitDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Principal;
+using System.Text;
+
+namespace Agile4SMB.Shared
+{
+    public class OrganizationUnitDTO
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+    }
+}
