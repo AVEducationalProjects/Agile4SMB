3ed7b67c5ad20a9bd8333f867689da37eecb177a
Александр Морозов <morozov@ilabsltd.com>
2020-06-24 12:17:31 +0400

    Base docker setuo

---
+++ src/Server/Controllers/OrganizationController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Agile4SMB.Shared;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Agile4SMB.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrganizationController : ControllerBase
+    {
+        public ActionResult<OrganizationUnitDTO> Get()
+        {
+            return Ok(null);
+        }
+    }
+}
