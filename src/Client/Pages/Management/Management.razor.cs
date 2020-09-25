1ca5f9d60494f1471eebd31c34235f55faabb118
V.Samarsky <v.samarsky97@gmail.com>
2020-09-25 21:24:40 +0400

    Management drop down choose Unit added

---
+++ src/Client/Pages/Management/Management.razor.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading.Tasks;
+using Agile4SMB.Client.Services;
+using Agile4SMB.Client.Utils;
+using Agile4SMB.Shared.Domain;
+using Microsoft.AspNetCore.Components;
+using Task = System.Threading.Tasks.Task;
+
+
+namespace Agile4SMB.Client.Pages.Management
+{
+    public class ManagementBase : ComponentBase, ISelectObserver<OrganizationUnit>
+    {
+        [Inject] public UserUnitService UserUnitService { get; set; }
+        
+        protected OrganizationUnit SelectedUnit { get; set; }
+        /// <summary>
+        /// current logged user
+        /// </summary>
+        public OrganizationUnit UserUnit { get; set; }
+        
+        protected override async Task OnInitializedAsync()
+        {
+            UserUnit = await UserUnitService.GetCurrentUnit();
+            Update();
+        }
+
+        protected override async Task OnParametersSetAsync()
+        {
+            UserUnit = await UserUnitService.GetCurrentUnit();
+        }
+
+        public OrganizationUnit Item => SelectedUnit;
+
+        public async Task Select(OrganizationUnit item)
+        {
+            UserUnit = await UserUnitService.GetCurrentUnit();
+            SelectedUnit = item;
+            Update();
+        }
+
+        public void Update()
+        {
+            StateHasChanged();
+        }
+    }
+
+}
